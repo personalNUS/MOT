c75c423642e03bdd3d7b4b4f05130df1468bb0f3
SherisseTJW <dev@github.com>
2022-10-05 10:02:43 +0800

    wip: model prediction code logic

--- model/model.py
+++ model/model.py
@@ -3,6 +3,7 @@ import cv2
 import tensorflow as tf
 
 IMG_DIM = (416, 416)
+CONFIDENCE_THRESHOLD = 0.5
 
 class Model:
    @staticmethod
@@ -23,10 +24,34 @@ class Model:
       inp = cv2.cvtColor(inp, cv2.COLOR_BGR2RGB)
       inp_tensor = tf.convert_to_tensor(inp, dtype = tf.uint8)
       inp_tensor = tf.expand_dims(inp_tensor, 0)
+      inp_tensor = inp_tensor / 255
 
       return inp_tensor
 
    @staticmethod
-   def predictOnLocalImg(imgFilePath):
+   def drawBoundingBox(coords):
+      print()
+
+   @staticmethod
+   def predictOnLocalImg(od_model, imgFilePath):
       img = cv2.imread(imgFilePath)
       inp_tensor = Model.processImg(img) 
+
+      preds = od_model.predict(inp_tensor)[0]
+      class_ids = []
+      confidences = []
+      boxes = []
+
+      rows = preds.shape[0]
+
+      img_width, img_height, _ = img.shape
+      x = img_width / IMG_DIM[0]
+      y = img_height / IMG_DIM[1]
+
+      for r in range(0, rows):
+         row = preds[r]
+         confidence = row[4]
+
+         if confidence >= CONFIDENCE_THRESHOLD:
+            scores = row[5:]
+
